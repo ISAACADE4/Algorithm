d2e1390d50b0efbbabe09a9fff0481584eb10fb0
ᗷOᒍᗩᑕK <129832208+ISAACADE4@users.noreply.github.com>
2024-06-04 13:39:48 +0100

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,11 @@
+public class Swap
+{public static void main(String[]args){
+    int number =78;
+    int anotherNumber=32;
+    int temp=number;
+    number=anotherNumber;
+
+    System.out.println(number);
+    System.out.println(anotherNumber);
+}
+}
